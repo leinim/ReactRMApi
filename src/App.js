68ad0facf6f838647c79344763630467f07e832f
leinim <lingua.milena@gmail.com>
2023-07-26 18:25:01 -0300

    Comenzando con react-router en app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./components/public/Footer";
 import Main from "./components/public/Main";
 import { NavBar } from "./components/public/NavBar";
+import { Detail } from "./components/public/Detail";
+import { UserForm } from "./components/public/UserForm";
 
 function App() {
   return (
-    <div>
-      <NavBar />
+    <Router>
+      <div>
+        <NavBar />
 
-      <Main />
+        <Routes>
+          <Route exact path={"/"} element={<Main />} />
+          <Route exact path={"/details/:id"} element={<Detail/>} />
+          <Route exact path={"/user-form"} element={<UserForm/>} />
+        </Routes>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
